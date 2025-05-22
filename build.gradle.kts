562f69b49166d9ac3dfcf859670060e1978cdedb
Luiz Aguiar <6086+laguiar@users.noreply.github.com>
2025-05-22 22:13:23 +0200

    Dependencies update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ val junitVersion: String by project
 
 plugins {
     application
-    kotlin("jvm") version "2.1.20"
-    id("io.ktor.plugin") version "3.1.1"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
+    kotlin("jvm") version "2.1.21"
+    id("io.ktor.plugin") version "3.1.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21"
 }
 
 group = "io.github"
